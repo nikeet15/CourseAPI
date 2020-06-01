b303cc55b7b577ee0c9548094a098989fd3ca587
nikeet15 <kesharinikeet@gmail.com>
2020-06-01 22:15:53 +0530

    added some controllers

--- src/main/java/io/javabrains/springboot/hello/HelloController.java
+++ src/main/java/io/javabrains/springboot/hello/HelloController.java
@@ -1,8 +1,17 @@
 package io.javabrains.springboot.hello;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+// tells Spring to make this class a controller ie to give responses to requests
+
 public class HelloController {
 
+    @RequestMapping("/hello")
+    //this method is called when user visits /hello URL GET Request
+
+    public String sayHi(){
+        return "Hi";
+    }
 }
